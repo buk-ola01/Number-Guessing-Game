89c97d8e943664407d88560e3cb0aa58014623e9
Jimoh Abdulsamad <jimohsamad2005@gmail.com>
2022-10-25 10:50:42 +0100

    Updated next stage

--- Number Guessing Game/script.js
+++ Number Guessing Game/script.js
@@ -6,19 +6,41 @@ function NumberGuesserCheck() {
 
      let userScore = 0;
      var instruction = alert(
-          "We have selected a random number between 1 - 2,See if you can guess it"
+          "We have selected a random number from 1 - 2,See if you can guess it"
      );
 
      var Input = prompt("Guess the number");
 
      if (number == Input) {
           Number.isInteger(Input);
-          userScore = 1;
+          userScore += 1;
           alert(
                `you guessed the number ${number} correctly.You now have ${userScore} point`
           );
      } else if (number !== Input) {
           userScore = 0;
-          alert(`your guess is incorrect. Better Luck next time`);
+          alert(`your guess is incorrect. Game Over`);
+     }
+
+     if (userScore == 1) {
+          var no = [Math.floor(Math.random() * 3)];
+
+          let input = prompt("Enter a number in range 1-3 ");
+
+          if (input == no) {
+               userScore += 1;
+               alert(
+                    `You guessed the number right!!. you now have ${userScore} point`
+               );
+          } else if (input < no) {
+               alert("You guessed too low");
+          }
+          if (input > no) {
+               alert("You guessed too high");
+               console.log(input);
+          }
+     }
+     if (userScore == 0) {
+          console.log(user);
      }
 }
